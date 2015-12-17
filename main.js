0753762fd4a8e50c20a64fc82b96616cab38f554
Tyler Carter <tyler@tylercarter.org>
2015-12-17 14:12:13 -0800

    Moved match specific functions into helper object

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ window.nar = (function( window ){
   obj.getTeamEventAverage = function( team_key, event_key ) {
 
     var processEventAverage = function( data ) {
-      var sorted = separateMatches( data );
+      var sorted = MatchHelper.separateMatches( data );
       var results = {
-        'qm' : getMatchesAverage( team_key, sorted.qm ),
-        'ef' : getMatchesAverage( team_key, sorted.ef ),
-        'qf' : getMatchesAverage( team_key, sorted.qf ),
-        'sf' : getMatchesAverage( team_key, sorted.sf ),
-        'f'  : getMatchesAverage( team_key, sorted.f ),
+        'qm' : MatchHelper.getMatchesAverage( team_key, sorted.qm ),
+        'ef' : MatchHelper.getMatchesAverage( team_key, sorted.ef ),
+        'qf' : MatchHelper.getMatchesAverage( team_key, sorted.qf ),
+        'sf' : MatchHelper.getMatchesAverage( team_key, sorted.sf ),
+        'f'  : MatchHelper.getMatchesAverage( team_key, sorted.f ),
       }
       console.log( results );
     }
@@ -25,58 +25,58 @@ window.nar = (function( window ){
     obj.api.TBA.getTeamMatches( team_key, event_key, processEventAverage );
   }
 
-  function separateMatches( matches ) {
-    var results = {
-      "qm" : [],
-      "ef" : [],
-      "qf" : [],
-      "sf" : [],
-      "f"  : [],
-    };
+  var MatchHelper = {
+    'separateMatches' : function ( matches ) {
+      var results = {
+        "qm" : [],
+        "ef" : [],
+        "qf" : [],
+        "sf" : [],
+        "f"  : [],
+      };
 
-    matches.forEach( function( entry ) {
-      results[ entry.comp_level ].push(entry);
-    } );
+      matches.forEach( function ( entry ) {
+        results[ entry.comp_level ].push(entry);
+      } );
 
-    return results;
-  }
-
-  function findMatchTeamAlliance( team_key, match ) {
-    var blue_teams = match.alliances.blue.teams;
-    if ( blue_teams.indexOf( team_key ) !== -1 ) {
-      return 'blue';
-    }
-
-    var red_teams = match.alliances.red.teams;
-    if ( red_teams.indexOf( team_key ) !== -1 ) {
-      return 'red';
-    }
-
-    return 'none';
-  }
-
-  function getMatchesAverage( team_key, matches ) {
-    if ( matches.length < 1 ) {
-      return 0;
-    }
-
-    var total = 0;
-    var count = 0;
-    matches.forEach( function( match ) {
-
-      var alliance = findMatchTeamAlliance( team_key, match );
-      if ( alliance === "none" ) {
-        return;
+      return results;
+    },
+    'getMatchesAverage' : function ( team_key, matches ) {
+      if ( matches.length < 1 ) {
+        return 0;
       }
 
-      var score = match.alliances[alliance].score;
-      total += score;
-      count += 1;
+      var total = 0;
+      var count = 0;
+      matches.forEach( function( match ) {
 
-    } );
+        var alliance = MatchHelper.findMatchTeamAlliance( team_key, match );
+        if ( alliance === "none" ) {
+          return;
+        }
 
-    return total / count;
-  }
+        var score = match.alliances[alliance].score;
+        total += score;
+        count += 1;
+
+      } );
+
+      return total / count;
+    },
+    'findMatchTeamAlliance' : function ( team_key, match ) {
+      var blue_teams = match.alliances.blue.teams;
+      if ( blue_teams.indexOf( team_key ) !== -1 ) {
+        return 'blue';
+      }
+
+      var red_teams = match.alliances.red.teams;
+      if ( red_teams.indexOf( team_key ) !== -1 ) {
+        return 'red';
+      }
+
+      return 'none';
+    }
+  };
 
   return obj;
 })(window);
